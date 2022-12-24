154f805ab65aa5fe524fb3b546fe7fac029a3c78
Ксюшка <ksi2644459@gmail.com>
2022-12-24 23:14:43 +0300

    trying shapes

--- Musica/app/src/main/java/com/example/musica/AudioModel.java
+++ Musica/app/src/main/java/com/example/musica/AudioModel.java
@@ -3,12 +3,13 @@ package com.example.musica;
 import java.io.Serializable;
 
 public class AudioModel implements Serializable {
-    String path, title, duration;
-    public AudioModel(String path,String title, String duration)
+    String path, title, duration, song_text;
+    public AudioModel(String path,String title, String duration, String )
     {
         this.path = path;
         this.title = title;
         this.duration = duration;
+        this.song_text = songtext;
 
     }
     public String getPath()
